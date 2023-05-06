6afe9d85e6d5b83a9ef8c8ffd1cc1f8a0446f8f0
lt_taozi <1764780379@qq.com>
2023-05-06 21:30:02 +0800

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -20,4 +20,4 @@ const val ktorVersion = "2.2.4"//ktor版本
 const val serializationJsonVersion = "1.5.0"//json序列号版本
 const val kspVersion = "$kotlinVersion-1.0.9"//ksp版本
 
-const val mVersion = "1.0.5"//此库的版本
+const val mVersion = "1.0.6"//此库的版本
